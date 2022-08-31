13a44879a5a6c61c0ac363cedf6c3e539cf8f191
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-01 06:55:00 +0900

    working prototype

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>App icon creator</h1>
+
+    <div class="">
+      <input type="file" @change="image_changed($event)">
+      <input type="checkbox" v-model="logo">
+      <label for="">Draw logo</label>
+    </div>
+
+    <div
+      class="images_wrapper"
+      v-if="image">
+      <AppIcon
+        :image="image"
+        v-for="(techno, index) in technos"
+        :key="index"
+        :techno="techno"
+        :logo="logo"/>
+    </div>
+
+
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AppIcon from '@/components/AppIcon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppIcon
+  },
+  data(){
+    return {
+      image: null,
+      logo: false,
+      technos: [
+        require('@/assets/helm.png'),
+        require('@/assets/vuetify.png'),
+        require('@/assets/vue.png'),
+        require('@/assets/pdf.png'),
+        null,
+      ],
+    }
+  },
+  methods:{
+    image_changed(event){
+      this.image = null
+      setTimeout( () => {
+        this.image = event.target.files[0]
+      }, 10)
+
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.images_wrapper {
+
 }
+
 </style>
---
+++ src/components/AppIcon.vue
@@ -0,0 +1,139 @@
+<template>
+  <canvas
+    ref="canvas"/>
+</template>
+
+<script>
+export default {
+  name: 'AppIcon',
+  props: {
+    techno: String,
+    image: File,
+    logo: Boolean,
+  },
+  data(){
+    return {
+
+      techno_lookup: {
+
+      },
+
+      context: null,
+      canvas: null,
+    }
+  },
+  mounted(){
+    this.get_canvas_and_context()
+    this.draw_base_image()
+
+
+
+
+
+
+
+
+
+
+
+  },
+  methods: {
+    get_canvas_and_context() {
+      this.canvas = this.$refs.canvas
+      this.context  = this.canvas.getContext('2d')
+    },
+    draw_base_image(){
+      const img = new Image()
+      img.src = URL.createObjectURL(this.image)
+
+      img.addEventListener('load', () => {
+
+        const img_min_dims = Math.max(img.width,img.height)
+
+        // Canvas should be square
+        this.canvas.width = img_min_dims
+        this.canvas.height = this.canvas.width
+
+        const img_corner = {
+          x: 0.5 * this.canvas.width - 0.5 * img.width,
+          y:  0.5 * this.canvas.height - 0.5 * img.height,
+        }
+
+        this.context.drawImage(img, img_corner.x, img_corner.y)
+        this.draw_techno()
+        if(this.logo) this.draw_logo()
+
+      }, false)
+    },
+    draw_logo(){
+
+      const {width, height} = this.canvas
+
+      const ratio = 0.15
+      const offset = 0.05 * width
+
+      const center = {x: (1 - ratio) * width, y: ratio * height}
+
+      const img_width = (2 * ratio) * width
+
+      const corner = {
+        x: center.x - 0.5 * img_width - offset,
+        y: center.y - 0.5 * img_width + offset}
+
+      const img = new Image()
+      img.src = require('@/assets/logo.png')
+      img.addEventListener('load', () => {
+
+        this.context.drawImage(img, corner.x, corner.y, img_width, img_width)
+
+      }, false)
+
+
+    },
+    draw_techno(){
+
+      const src = this.techno
+      if(!src) return
+
+      const {width, height} = this.canvas
+
+
+
+      const ratio = 0.2
+      const inverse_ratio = 1 - ratio
+      const offset = 0 * width
+
+      const radius = ratio * width
+      const center = {
+        x: ratio * width + offset,
+        y: inverse_ratio * height - offset
+      }
+
+      const img_width = 0.55 * 2 * radius
+
+      const corner = {x: center.x - 0.5 * img_width, y: center.y - 0.5 * img_width}
+
+      const img = new Image()
+      img.src = src
+      img.addEventListener('load', () => {
+
+        this.context.arc(center.x, center.y, radius, radius, 0, 2 * Math.PI, false)
+        this.context.fillStyle = 'white'
+        this.context.fill()
+
+        this.context.drawImage(img, corner.x, corner.y, img_width, img_width)
+
+      }, false)
+
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+canvas {
+  border: 1px solid #dddddd;
+  margin: 1em;
+}
+</style>
