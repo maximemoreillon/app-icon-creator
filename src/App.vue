7772a24cf29fc16535f2e420f6c2f39d505d87c7
Maxime Moreillon <m.moreillon@gmail.com>
2022-10-29 12:22:22 +0900

    fixed missing beginpath

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,22 @@
 
       <canvas class="output" ref="canvas" />
 
-      <div v-for="(image, index) in images" :key="index" :class="image.label" :style="{ 'grid-area': image.label}">
-        <input type="file" @change="imageChanged(index, $event)" accept="image/*" >
+      <div style="grid-area: main;">
+        <ImageInput v-model="main.file" />
       </div>
-    </div>
 
-    
-
-    <div
-      class="images_wrapper"
-      v-if="image">
-
-      <canvas ref="canvas" />
+      <div v-for="(image, index) in corners" :key="index" :class="image.label" :style="{ 'grid-area': image.label}">
+        <!-- <input type="file" @change="imageChanged(image, $event)" accept="image/*" > -->
+        <ImageInput v-model="image.file"/>
+      </div>
 
     </div>
+
+    <div style="grid-area: button;">
+      <button @click="draw_icon()">Draw</button>
+    </div>
+
+  
 
 
 
@@ -33,36 +35,43 @@
 <script>
 
 // import AppIcon from '@/components/AppIcon.vue'
+import ImageInput from '@/components/ImageInput.vue'
 
 export default {
   name: 'App',
   components: {
-    // AppIcon
+    ImageInput
   },
   data(){
     return {
       image: null,
 
-      images: [
-        {
-          label: 'main',
-          file: null,
-        },
+      main: {
+        label: 'main',
+        file: null,
+      },
+
+      corners: [
+
         {
           label: 'top-left',
           file: null,
+          drawBackground: true,
         },
         {
           label: 'top-right',
           file: null,
+          drawBackground: true,
         },
         {
           label: 'bottom-left',
           file: null,
+          drawBackground: true,
         },
         {
           label: 'bottom-right',
           file: null,
+          drawBackground: true,
         },
       ],
 
@@ -79,101 +88,103 @@ export default {
       this.canvas = this.$refs.canvas
       this.context = this.canvas.getContext('2d')
     },
-    imageChanged(index, event){
-      setTimeout( () => {
-        this.images[index].file = event.target.files[0]
-        this.draw_icon()
 
+    async draw_icon(){
+      this.clear_canvas()
+      await this.draw_main_image()
+      this.corners.forEach( corner => { this.draw_corner(corner) })
 
-      }, 10)
 
     },
-    draw_icon(){
-      this.draw_main_image()
-      for (let i=1; i<=4; i++) {
-        this.draw_corner(i)
+
+    loadImage(image){
+      return new Promise ((resolve) => {
+        const img = new Image()
+        img.src = URL.createObjectURL(image)
+
+        img.addEventListener('load', () => {
+          resolve(img)
+        }, false)
+      })
+    },
+    wait(delay){
+      return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+      })
+    },
+    clear_canvas(){
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    },
+    async draw_main_image() {
+      const image = this.main.file
+      if(!image) return
+
+      const img = await this.loadImage(image)
+
+      const img_min_dims = Math.max(img.width, img.height)
+
+      // Canvas must be square
+      this.canvas.width = img_min_dims
+      this.canvas.height = img_min_dims
+
+      // Center image in canvas
+      const imgTopLeftCorner = {
+        x: 0.5 * this.canvas.width - 0.5 * img.width,
+        y: 0.5 * this.canvas.height - 0.5 * img.height,
       }
-    },
-    draw_main_image() {
-      const image = this.images[0].file
-      if(!image) return
-      const img = new Image()
-      img.src = URL.createObjectURL(image)
 
-      img.addEventListener('load', () => {
-
-        const img_min_dims = Math.max(img.width, img.height)
-
-        // Canvas should be square
-        this.canvas.width = img_min_dims
-        this.canvas.height = img_min_dims
-
-        // Center image in canvas
-        const img_corner = {
-          x: 0.5 * this.canvas.width - 0.5 * img.width,
-          y: 0.5 * this.canvas.height - 0.5 * img.height,
-        }
-
-        this.context.drawImage(img, img_corner.x, img_corner.y)
-      }, false)
+      this.context.drawImage(img, imgTopLeftCorner.x, imgTopLeftCorner.y)
 
     },
-    draw_corner(index) {
-      if(index === 0) return
-      const image = this.images[index]
-      if(!image) return
-
-      const {file, label} = image
 
 
-
+    async draw_corner(corner) {
+      
+      const {file, label} = corner
 
       if (!file) return
 
       console.log(`Corner ${label} has image`)
       
 
-      const img = new Image()
-      img.src = URL.createObjectURL(file)
-      img.addEventListener('load', () => {
-
-        //canvas width and height should be the same
-        const { width: canvasWidth, height: canvasHeight } = this.canvas
+      const img = await this.loadImage(file)
 
 
-        // TODO: Preserve aspect radio of original image
-        const img_min_dims = Math.max(img.width, img.height)
-
-        console.log(img_min_dims)
+      //canvas width and height should be the same
+      const { width: canvasWidth, height: canvasHeight } = this.canvas
 
 
-        let imageCenter
+      // TODO: Preserve aspect radio of original image
+      // const img_min_dims = Math.max(img.width, img.height)
 
-        const ratio = 0.2
-        const inverse_ratio = 1 - ratio
+      
+      const ratio = 0.2
+      const inverse_ratio = 1 - ratio
+      
+      const radius = ratio * canvasWidth
+      const imageMaxHeightOrWidth = 0.55 * 2 * radius
+      
+      let imageCenter
 
-        const radius = ratio * canvasWidth
-        const imageMaxHeightOrWidth = 0.55 * 2 * radius
+      if (label === 'top-left') imageCenter = { x: ratio * canvasWidth, y: ratio * canvasHeight }
+      if (label === 'bottom-left') imageCenter = { x: ratio * canvasWidth, y: inverse_ratio * canvasHeight }
+      if (label === 'top-right') imageCenter = { x: inverse_ratio * canvasWidth, y: ratio * canvasHeight }
+      if (label === 'bottom-right') imageCenter = { x: inverse_ratio * canvasWidth, y: inverse_ratio * canvasHeight }
 
+      const imageCorner = { x: imageCenter.x - 0.5 * imageMaxHeightOrWidth, y: imageCenter.y - 0.5 * imageMaxHeightOrWidth }
 
-
-
-        if (label === 'top-left') imageCenter = { x: ratio * canvasWidth, y: ratio * canvasHeight }
-        if (label === 'bottom-left') imageCenter = { x: ratio * canvasWidth, y: inverse_ratio * canvasHeight }
-        if (label === 'top-right') imageCenter = { x: inverse_ratio * canvasWidth, y: ratio * canvasHeight }
-        if (label === 'bottom-right') imageCenter = { x: inverse_ratio * canvasWidth, y: inverse_ratio * canvasHeight }
-
-        const imageCorner = { x: imageCenter.x - 0.5 * imageMaxHeightOrWidth, y: imageCenter.y - 0.5 * imageMaxHeightOrWidth }
-
-        // Draw a white background around image
+      // Draw a white background around image
+      if (corner.drawBackground) {
+        this.context.beginPath()
         this.context.arc(imageCenter.x, imageCenter.y, radius, radius, 0, 2 * Math.PI, false)
         this.context.fillStyle = 'white'
         this.context.fill()
 
-        // Draw the image
-        this.context.drawImage(img, imageCorner.x, imageCorner.y, imageMaxHeightOrWidth, imageMaxHeightOrWidth)
+      }
 
-      }, false)
+      // Draw the image
+      this.context.drawImage(img, imageCorner.x, imageCorner.y, imageMaxHeightOrWidth, imageMaxHeightOrWidth)
 
 
     }
@@ -189,12 +200,9 @@ export default {
     ' top-left . top-right'
     '. output .'
     '. main .'
-    'bottom-left . bottom-right';
+    'bottom-left button bottom-right';
 }
 
-.main {
-  grid-area: main;
-}
 
 .output {
   grid-area: output;
---
+++ src/components/ImageInput.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="imageInput">
+        <img v-if="image" :src="imageurl" alt="">
+        <input type="file" @change="fileChanged($event)" accept="image/*">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ImageInput',
+    props: {
+        value: undefined
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        
+
+    },
+    methods: {
+        fileChanged(event) {
+            this.image = event.target.files[0]
+        },
+    },
+    computed: {
+        image: {
+            get(){ return this.value },
+            set(newVal){ this.$emit('input', newVal)}
+        },
+        imageurl(){
+            if(!this.image) return
+            return URL.createObjectURL(this.image)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.imageInput {
+    display: flex;
+    flex-direction: column;
+}
+
+img {
+    width: 5rem;
+    height: 5rem;
+    object-fit: contain;
+}
+</style>
