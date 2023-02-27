3341e6583a33d9dce89acc3c8eb7e98db5ca6aea
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-27 19:15:13 +0900

    optional background

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app>
-
     <v-app-bar app dark color="#444444">
       <v-toolbar-title>Icon maker</v-toolbar-title>
     </v-app-bar>
 
     <v-main class="grey lighten-4">
-      
       <v-container>
         <v-row>
           <v-col cols="12" md="6">
-            <v-card >
+            <v-card>
               <v-card-title>Input</v-card-title>
               <v-card-text class="inputWrapper">
-                <ImageManagement v-model="images[index]" v-for="(image, index) in images" :key="index" />
+                <ImageManagement
+                  v-model="images[index]"
+                  v-for="(image, index) in images"
+                  :key="index"
+                />
               </v-card-text>
             </v-card>
           </v-col>
@@ -33,42 +35,40 @@
 </template>
 
 <script>
-
-import ImageManagement from '@/components/ImageManagement.vue'
+import ImageManagement from "@/components/ImageManagement.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ImageManagement
+    ImageManagement,
   },
   data() {
     return {
       image: null,
 
-
-
       images: [
         {
-          label: 'main',
+          label: "main",
           file: null,
+          drawBackground: false,
         },
         {
-          label: 'top-left',
-          file: null,
-          drawBackground: true,
-        },
-        {
-          label: 'top-right',
+          label: "top-left",
           file: null,
           drawBackground: true,
         },
         {
-          label: 'bottom-left',
+          label: "top-right",
           file: null,
           drawBackground: true,
         },
         {
-          label: 'bottom-right',
+          label: "bottom-left",
+          file: null,
+          drawBackground: true,
+        },
+        {
+          label: "bottom-right",
           file: null,
           drawBackground: true,
         },
@@ -76,7 +76,6 @@ export default {
 
       context: null,
       canvas: null,
-
     }
   },
   mounted() {
@@ -93,16 +92,16 @@ export default {
   methods: {
     get_canvas_and_context() {
       this.canvas = this.$refs.canvas
-      this.context = this.canvas.getContext('2d')
+      this.context = this.canvas.getContext("2d")
     },
 
     async draw_icon() {
       const [mainImage, ...corners] = this.images
       this.clear_canvas()
       await this.draw_main_image(mainImage)
-      corners.forEach(corner => { this.draw_corner(corner) })
-
-
+      corners.forEach((corner) => {
+        this.draw_corner(corner)
+      })
     },
 
     loadImage(image) {
@@ -110,15 +109,18 @@ export default {
         const img = new Image()
         img.src = URL.createObjectURL(image)
 
-        img.addEventListener('load', () => {
-          resolve(img)
-        }, false)
+        img.addEventListener(
+          "load",
+          () => {
+            resolve(img)
+          },
+          false
+        )
       })
     },
 
     clear_canvas() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
     },
     async draw_main_image(mainImage) {
       const image = mainImage.file
@@ -134,17 +136,22 @@ export default {
 
       // Center image in canvas
       const imgTopLeftCorner = {
-        x: 0.5 * this.canvas.width - 0.5 * img.width,
-        y: 0.5 * this.canvas.height - 0.5 * img.height,
+        x: 0.5 * img_min_dims - 0.5 * img.width,
+        y: 0.5 * img_min_dims - 0.5 * img.height,
+      }
+
+      if (mainImage.drawBackground) {
+        this.context.fillStyle = "white"
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        // this.context.beginPath()
+        // this.context.roundRect(0,0,img_min_dims,img_min_dims,0.1 * img_min_dims)
+        // this.context.fill()
       }
 
       this.context.drawImage(img, imgTopLeftCorner.x, imgTopLeftCorner.y)
-
     },
 
-
     async draw_corner(corner) {
-
       const { file, label } = corner
 
       if (!file) return
@@ -152,7 +159,6 @@ export default {
       const img = await this.loadImage(file)
 
       const { width: canvasWidth, height: canvasHeight } = this.canvas
-
 
       const ratio = 0.2
       const inverse_ratio = 1 - ratio
@@ -168,31 +174,57 @@ export default {
       const imageWidth = Math.min(imaginaryWidth, imageMaxHeightOrWidth)
       const imageHeight = Math.min(imaginaryHeight, imageMaxHeightOrWidth)
 
-
       // Compute center of image
       let imageCenter
-      if (label === 'top-left') imageCenter = { x: ratio * canvasWidth, y: ratio * canvasHeight }
-      if (label === 'bottom-left') imageCenter = { x: ratio * canvasWidth, y: inverse_ratio * canvasHeight }
-      if (label === 'top-right') imageCenter = { x: inverse_ratio * canvasWidth, y: ratio * canvasHeight }
-      if (label === 'bottom-right') imageCenter = { x: inverse_ratio * canvasWidth, y: inverse_ratio * canvasHeight }
+      if (label === "top-left")
+        imageCenter = { x: ratio * canvasWidth, y: ratio * canvasHeight }
+      if (label === "bottom-left")
+        imageCenter = {
+          x: ratio * canvasWidth,
+          y: inverse_ratio * canvasHeight,
+        }
+      if (label === "top-right")
+        imageCenter = {
+          x: inverse_ratio * canvasWidth,
+          y: ratio * canvasHeight,
+        }
+      if (label === "bottom-right")
+        imageCenter = {
+          x: inverse_ratio * canvasWidth,
+          y: inverse_ratio * canvasHeight,
+        }
 
-      const imageCorner = { x: imageCenter.x - 0.5 * imageWidth, y: imageCenter.y - 0.5 * imageHeight }
+      const imageCorner = {
+        x: imageCenter.x - 0.5 * imageWidth,
+        y: imageCenter.y - 0.5 * imageHeight,
+      }
 
       // Draw a white background around image
       if (corner.drawBackground) {
         this.context.beginPath()
-        this.context.arc(imageCenter.x, imageCenter.y, radius, radius, 0, 2 * Math.PI, false)
-        this.context.fillStyle = 'white'
+        this.context.arc(
+          imageCenter.x,
+          imageCenter.y,
+          radius,
+          radius,
+          0,
+          2 * Math.PI,
+          false
+        )
+        this.context.fillStyle = "white"
         this.context.fill()
       }
 
       // Draw the image
-      this.context.drawImage(img, imageCorner.x, imageCorner.y, imageWidth, imageHeight)
-
-
-    }
-  }
-
+      this.context.drawImage(
+        img,
+        imageCorner.x,
+        imageCorner.y,
+        imageWidth,
+        imageHeight
+      )
+    },
+  },
 }
 </script>
 
@@ -201,15 +233,12 @@ export default {
   display: grid;
   gap: 1em;
   grid-template-areas:
-    ' top-left . top-right'
-    '. main .'
-    'bottom-left . bottom-right';
+    " top-left . top-right"
+    ". main ."
+    "bottom-left . bottom-right";
   /* grid-template-rows: 1fr 1.5fr 1fr; */
   grid-template-columns: 1fr 1.5fr 1fr;
-
 }
-
-
 
 .output {
   width: 100%;
--- src/components/ImageManagement.vue
+++ src/components/ImageManagement.vue
@@ -1,94 +1,84 @@
 <template>
-    <v-card 
-        outlined
-        :class="image.label" 
-        :style="{ 'grid-area': image.label }"
-        class="imageWrapper">
+  <v-card
+    outlined
+    :class="image.label"
+    :style="{ 'grid-area': image.label }"
+    class="imageWrapper"
+  >
+    <v-toolbar flat dense>
+      <v-btn v-if="image.file" @click="clearInput()" icon class="clearButton">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-file-input v-else hide-input accept="image/*" v-model="imageFile" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click="image.drawBackground = !image.drawBackground"
+        :color="image.drawBackground ? '#c00000' : undefined"
+      >
+        <v-icon>mdi-arrange-send-backward</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-divider />
 
-        <v-toolbar flat dense>
-            
-            <v-btn v-if="image.file" @click="clearInput()" icon class="clearButton">
-                <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-file-input v-else hide-input accept="image/*" v-model="imageFile" />
-            <v-spacer />
-            <v-btn 
-                v-if="image.label !== 'main'"
-                icon 
-                @click="image.drawBackground = !image.drawBackground" 
-                :color="image.drawBackground ? '#c00000' : undefined">
-                <v-icon>mdi-arrange-send-backward</v-icon>
-            </v-btn>
-            
-
-        </v-toolbar>
-        <v-divider />
-
-
-            
-        <v-card-text class="imagePreviewWrapper">
-            <img v-if="image.file" :src="imageurl" alt="" class="imagePreview">
-            <v-icon v-else size="48">mdi-image-off</v-icon>
-        </v-card-text>
-            
-
-    
-    </v-card>
+    <v-card-text class="imagePreviewWrapper">
+      <img v-if="image.file" :src="imageurl" alt="" class="imagePreview" />
+      <v-icon v-else size="48">mdi-image-off</v-icon>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-
 export default {
-    name: 'ImageManagement',
-    props: {
-        value: undefined
-    },
-    components: {
-        
-    },
-    data() {
-        return {
-            imageFile: null
-        }
-    },
-    watch: {
-        imageFile(){
-            this.image.file = this.imageFile
-        }
-    },
-    methods: {
-
-        clearInput() {
-            this.imageFile = null
-            this.image.file = null
-        }
-    },
-    computed: {
-        image: {
-            get() { return this.value },
-            set(newVal) { this.$emit('input', newVal) }
-        },
-        imageurl() {
-            if (!this.image) return
-            return URL.createObjectURL(this.image.file)
-        }
+  name: "ImageManagement",
+  props: {
+    value: undefined,
+  },
+  components: {},
+  data() {
+    return {
+      imageFile: null,
     }
+  },
+  watch: {
+    imageFile() {
+      this.image.file = this.imageFile
+    },
+  },
+  methods: {
+    clearInput() {
+      this.imageFile = null
+      this.image.file = null
+    },
+  },
+  computed: {
+    image: {
+      get() {
+        return this.value
+      },
+      set(newVal) {
+        this.$emit("input", newVal)
+      },
+    },
+    imageurl() {
+      if (!this.image) return
+      return URL.createObjectURL(this.image.file)
+    },
+  },
 }
 </script>
 
 <style scoped>
-
 .imagePreviewWrapper {
-    grid-area: imagePreviewWrapper;
-    display: flex;
-    justify-content: center;
+  grid-area: imagePreviewWrapper;
+  display: flex;
+  justify-content: center;
 }
 .imagePreview {
-    grid-area: imagePreview;
-    object-fit: contain;
-    align-self: center;
-    width: 100%;
-    height: 100%;
+  grid-area: imagePreview;
+  object-fit: contain;
+  align-self: center;
+  width: 100%;
+  height: 100%;
 }
-
 </style>
